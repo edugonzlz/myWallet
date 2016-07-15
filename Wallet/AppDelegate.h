1c5b45f5dfacccd45a86cbd5cc979fb857af712a
Eduardo González <fonologico@gmail.com>
2016-07-15 22:30:23 +0200

    Initial Commit

---
+++ Wallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wallet
+//
+//  Created by Edu González on 15/7/16.
+//  Copyright © 2016 Edu González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
